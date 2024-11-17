05f0de4570ed99d544b9e21276c9c96a94e3c804
Cyclopropinon <cyclopropinon@realprogrammers.org>
2024-11-17 23:10:01 +0100

    make Timer.h cross-platform using ctime

--- NonEuclidean/Timer.h
+++ NonEuclidean/Timer.h
@@ -1,28 +1,29 @@
 #pragma once
-#include <Windows.h>
+#include <ctime>
 
 class Timer {
 public:
   Timer() {
-    QueryPerformanceFrequency(&frequency);
+    // Using CLOCK_MONOTONIC for high-resolution and monotonic time
+    clock_getres(CLOCK_MONOTONIC, &frequency);
   }
 
   void Start() {
-    QueryPerformanceCounter(&t1);
+    clock_gettime(CLOCK_MONOTONIC, &t1);
   }
 
   float Stop() {
-    QueryPerformanceCounter(&t2);
-    return float(t2.QuadPart - t1.QuadPart) / frequency.QuadPart;
+    clock_gettime(CLOCK_MONOTONIC, &t2);
+    return float((t2.tv_sec - t1.tv_sec) * 1e9 + (t2.tv_nsec - t1.tv_nsec)) / 1e9;
   }
 
   int64_t GetTicks() {
-    QueryPerformanceCounter(&t2);
-    return t2.QuadPart;
+    clock_gettime(CLOCK_MONOTONIC, &t2);
+    return (t2.tv_sec * 1e9 + t2.tv_nsec);
   }
 
   int64_t SecondsToTicks(float s) {
-    return int64_t(float(frequency.QuadPart) * s);
+    return int64_t(s * 1e9); // Convert seconds to nanoseconds
   }
 
   float StopStart() {
@@ -32,6 +33,6 @@ public:
   }
 
 private:
-  LARGE_INTEGER frequency;        // ticks per second
-  LARGE_INTEGER t1, t2;           // ticks
+  struct timespec frequency; // Resolution of the clock
+  struct timespec t1, t2;    // Start and stop times
 };
